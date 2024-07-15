3a181bfaf3654bd741092dfb77e6a77ee6a724d6
Isuru Ranawaka <irjanith@gmail.com>
2024-07-15 08:26:17 -0400

    Adding matrix multiplication time

--- multiply.cpp
+++ multiply.cpp
@@ -2,6 +2,7 @@
 #include <Teuchos_RCP.hpp>
 #include <Tpetra_Core.hpp>
 #include <Tpetra_CrsMatrix.hpp>
+#include <Tpetra_CrsGraph.hpp>
 #include <Tpetra_Import.hpp>
 #include <MatrixMarket_Tpetra.hpp>
 #include <TpetraExt_MatrixMatrix.hpp>
@@ -9,6 +10,7 @@
 #include <Teuchos_CommHelpers.hpp>
 #include <Teuchos_TimeMonitor.hpp>
 #include <iostream>
+#include <stdexcept>
 
 int main(int argc, char* argv[]) {
   // Initialize MPI
@@ -34,15 +36,47 @@ int main(int argc, char* argv[]) {
   using node_type = Tpetra::Map<>::node_type;
 
   using crs_matrix_type = Tpetra::CrsMatrix<scalar_type, local_ordinal_type, global_ordinal_type, node_type>;
+  using crs_graph_type = Tpetra::CrsGraph<local_ordinal_type, global_ordinal_type, node_type>;
   using map_type = Tpetra::Map<local_ordinal_type, global_ordinal_type, node_type>;
 
   Teuchos::RCP<const map_type> mapA;
   Teuchos::RCP<crs_matrix_type> A;
-  A = Tpetra::MatrixMarket::Reader<crs_matrix_type>::readSparseFile(matrixAPath, comm);
-
   Teuchos::RCP<const map_type> mapB;
   Teuchos::RCP<crs_matrix_type> B;
-  B = Tpetra::MatrixMarket::Reader<crs_matrix_type>::readSparseFile(matrixBPath, comm);
+
+  try {
+    // Attempt to read matrix A as CrsMatrix
+    A = Tpetra::MatrixMarket::Reader<crs_matrix_type>::readSparseFile(matrixAPath, comm);
+  } catch (const std::invalid_argument& e) {
+    // If reading as CrsMatrix fails, try reading as CrsGraph
+    try {
+      Teuchos::RCP<crs_graph_type> graphA = Tpetra::MatrixMarket::Reader<crs_graph_type>::readSparseGraph(matrixAPath, comm);
+      mapA = graphA->getRowMap();
+      A = Teuchos::rcp(new crs_matrix_type(graphA));
+    } catch (const std::exception& e_graph) {
+      if (comm->getRank() == 0) {
+        std::cerr << "Error reading matrix A from file: " << e_graph.what() << std::endl;
+      }
+      return -1;
+    }
+  }
+
+  try {
+    // Attempt to read matrix B as CrsMatrix
+    B = Tpetra::MatrixMarket::Reader<crs_matrix_type>::readSparseFile(matrixBPath, comm);
+  } catch (const std::invalid_argument& e) {
+    // If reading as CrsMatrix fails, try reading as CrsGraph
+    try {
+      Teuchos::RCP<crs_graph_type> graphB = Tpetra::MatrixMarket::Reader<crs_graph_type>::readSparseGraph(matrixBPath, comm);
+      mapB = graphB->getRowMap();
+      B = Teuchos::rcp(new crs_matrix_type(graphB));
+    } catch (const std::exception& e_graph) {
+      if (comm->getRank() == 0) {
+        std::cerr << "Error reading matrix B from file: " << e_graph.what() << std::endl;
+      }
+      return -1;
+    }
+  }
 
   // Check dimensions
   if (A->getGlobalNumCols() != B->getGlobalNumRows()) {

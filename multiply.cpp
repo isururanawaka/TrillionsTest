03d3cd0a96d9358afaaa6ddf35ba162ad39e360c
Isuru Ranawaka <irjanith@gmail.com>
2024-07-15 09:20:06 -0400

    Adding matrix multiplication time

--- multiply.cpp
+++ multiply.cpp
@@ -4,23 +4,21 @@
 #include <Tpetra_Map.hpp>
 #include <Tpetra_MultiVector.hpp>
 #include <Teuchos_RCP.hpp>
-#include <Teuchos_DefaultComm.hpp>
 #include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_DefaultComm.hpp>
 #include <Teuchos_TimeMonitor.hpp>
 #include <iostream>
 
-typedef Tpetra::DefaultPlatform::DefaultPlatformType::NodeType node_type;
 typedef double scalar_type;
 typedef int local_ordinal_type;
 typedef long long global_ordinal_type;
-
-typedef Tpetra::CrsMatrix<scalar_type, local_ordinal_type, global_ordinal_type, node_type> crs_matrix_type;
-typedef Tpetra::CrsGraph<local_ordinal_type, global_ordinal_type, node_type> crs_graph_type;
-typedef Tpetra::Map<local_ordinal_type, global_ordinal_type, node_type> map_type;
+typedef Tpetra::CrsMatrix<scalar_type, local_ordinal_type, global_ordinal_type> crs_matrix_type;
+typedef Tpetra::CrsGraph<local_ordinal_type, global_ordinal_type> crs_graph_type;
+typedef Tpetra::Map<local_ordinal_type, global_ordinal_type> map_type;
 
 int main(int argc, char *argv[]) {
   Teuchos::GlobalMPISession mpiSession(&argc, &argv);
-  Teuchos::RCP<const Teuchos::Comm<int>> comm = Tpetra::DefaultPlatform::getDefaultPlatform().getComm();
+  Teuchos::RCP<const Teuchos::Comm<int>> comm = Teuchos::DefaultComm<int>::getComm();
 
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " matrixAPath matrixBPath" << std::endl;
@@ -51,7 +49,7 @@ int main(int argc, char *argv[]) {
   Teuchos::RCP<crs_matrix_type> C;
   {
     Teuchos::TimeMonitor tm(*Teuchos::TimeMonitor::getNewTimer("Matrix Multiplication Time"));
-    C = Tpetra::MatrixMatrix::multiply(*A, false, *B, false);
+    Tpetra::MatrixMatrix::Multiply(*A, false, *B, false, C);
   }
 
   // Print the timing results
